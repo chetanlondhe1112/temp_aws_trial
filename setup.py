fc99e1515375aed1bc78731bb13fb24606608bad
chetan <chetanlondhe1112@gmail.com>
2023-03-04 18:54:26 +0530

    Update 0.1 Changes to be committed:     new file:   setup.py 	new file:   sqls/stocks_dashboard.sql

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='temp_aws_trial',
+    version='v0.0',
+    packages=find_packages(),
+    url='',
+    license='MIT',
+    author='Chetan Londhe',
+    author_email='chetanlondhe1112@gmail.com',
+    description='Trial on aws ec2 instance database and streamlit connection '
+)
